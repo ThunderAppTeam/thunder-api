94c8b3c823f4204b4810f6846e0a4964d6e495ef
hei5enbug <kbm0598@gmail.com>
2025-01-08 03:20:43 +0900

    fix: change ExceptionHandler(Exception::class -> RuntimeException::class)

--- src/main/kotlin/app/thunder/api/controller/GlobalControllerAdvice.kt
+++ src/main/kotlin/app/thunder/api/controller/GlobalControllerAdvice.kt
@@ -1,48 +1,40 @@
 package app.thunder.api.controller
 
 import app.thunder.api.controller.response.ErrorResponse
-import app.thunder.api.controller.response.SuccessResponse
 import app.thunder.api.exception.CommonErrors.UNKNOWN_SERVER_ERROR
 import app.thunder.api.exception.ThunderException
 import jakarta.servlet.http.HttpServletRequest
 import org.slf4j.LoggerFactory
-import org.springframework.core.MethodParameter
-import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.http.converter.HttpMessageConverter
-import org.springframework.http.server.ServerHttpRequest
-import org.springframework.http.server.ServerHttpResponse
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
 
 @RestControllerAdvice
-class GlobalControllerAdvice : ResponseBodyAdvice<Any> {
+class GlobalControllerAdvice {
 
     private final val logger = LoggerFactory.getLogger(this.javaClass)
 
-    override fun supports(returnType: MethodParameter, converterType: Class<out HttpMessageConverter<*>>): Boolean {
-        return true
-    }
-
-    override fun beforeBodyWrite(
-        body: Any?,
-        returnType: MethodParameter,
-        mediaType: MediaType,
-        selectedConverterType: Class<out HttpMessageConverter<*>>,
-        request: ServerHttpRequest,
-        response: ServerHttpResponse
-    ): Any? {
-        if (body is SuccessResponse<*> || body is ErrorResponse || returnType.method?.name == HttpMethod.OPTIONS.name()) {
-            return body
-        }
-
-        return SuccessResponse(
-            data = body,
-            path = request.uri.path
-        )
-    }
+//    override fun supports(returnType: MethodParameter, converterType: Class<out HttpMessageConverter<*>>): Boolean {
+//        return true
+//    }
+//
+//    override fun beforeBodyWrite(
+//        body: Any?,
+//        returnType: MethodParameter,
+//        mediaType: MediaType,
+//        selectedConverterType: Class<out HttpMessageConverter<*>>,
+//        request: ServerHttpRequest,
+//        response: ServerHttpResponse
+//    ): Any? {
+//        if (body is SuccessResponse<*> || body is ErrorResponse || returnType.method?.name == HttpMethod.OPTIONS.name()) {
+//            return body
+//        }
+//
+//        return SuccessResponse(
+//            data = body,
+//            path = request.uri.path
+//        )
+//    }
 
     @ExceptionHandler(ThunderException::class)
     fun handleThunderException(
@@ -57,7 +49,7 @@ class GlobalControllerAdvice : ResponseBodyAdvice<Any> {
         return ResponseEntity.status(ex.errorCode.httpStatus).body(errorResponse)
     }
 
-    @ExceptionHandler(Exception::class)
+    @ExceptionHandler(RuntimeException::class)
     fun handleException(
         ex: Exception,
         request: HttpServletRequest
