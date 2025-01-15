c35c1007efca9fbf9a1a32f669da27e637ee9d66
byoungmin <kbm0598@tradlinx.com>
2025-01-15 12:44:05 +0900

    fix: change 'data' field in response non-nullable

--- src/main/kotlin/app/thunder/api/controller/request/PostLoginResponse.kt
+++ src/main/kotlin/app/thunder/api/controller/request/PostLoginResponse.kt
@@ -1,8 +1,5 @@
 package app.thunder.api.controller.request
 
-import com.fasterxml.jackson.annotation.JsonInclude
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
 data class PostLoginResponse(
     val accessToken: String?
 )
--- src/main/kotlin/app/thunder/api/controller/response/SuccessResponse.kt
+++ src/main/kotlin/app/thunder/api/controller/response/SuccessResponse.kt
@@ -6,10 +6,15 @@ import java.time.ZonedDateTime
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class SuccessResponse<T>(
     val message: String = DEFAULT_SUCCESS_MESSAGE,
-    val data: T? = null,
+    val data: T? = EmptyResponse() as T?,
     val path: String,
     val timestamp: ZonedDateTime = ZonedDateTime.now()
 ) {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private data class EmptyResponse(
+        val empty: Nothing? = null
+    )
 
     companion object {
         const val DEFAULT_SUCCESS_MESSAGE = "Request completed successfully"
