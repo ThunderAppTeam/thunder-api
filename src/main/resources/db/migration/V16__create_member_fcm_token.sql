3dfbf4993966948405031b27023f3576f52c89d6
hei5enbug <kbm0598@gmail.com>
2025-02-25 19:49:58 +0900

    fix: drop unique key from fcm_token column in member_fcm_token table

--- src/main/resources/db/migration/V16__create_member_fcm_token.sql
+++ src/main/resources/db/migration/V16__create_member_fcm_token.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS member_fcm_token
 (
     member_id  BIGINT PRIMARY KEY,
-    fcm_token  TEXT      NOT NULL UNIQUE,
+    fcm_token  TEXT      NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
