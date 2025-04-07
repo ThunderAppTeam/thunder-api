1356207d99deaa2753ab22e2e2ee95cbbec663dd
hei5enbug <kbm0598@gmail.com>
2025-04-07 20:46:31 +0900

    fix: resolve jar path error in Dockerfile and optimize build config

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 FROM gradle:8.11-jdk21 AS builder
+ARG MODULE_NAME
 WORKDIR /app
 COPY . .
-RUN ./gradlew build --no-daemon
+RUN ./gradlew core:core-api:build -x test --no-daemon
 
 FROM openjdk:21-jdk-slim-buster
 WORKDIR /app
 
 ARG ENV_FILE_PATH
 
-COPY --from=builder /app/build/libs/thunder-api*.jar /app/thunder-api.jar
+COPY --from=builder /app/core/core-api/build/libs/*.jar /app/thunder-api.jar
 COPY ${ENV_FILE_PATH} /app/.env
 
 EXPOSE 9000
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     kotlin("jvm")
     kotlin("plugin.spring") apply false
     kotlin("plugin.jpa") apply false
     id("org.springframework.boot") apply false
-    id("io.spring.dependency-management")
+    id("io.spring.dependency-management") apply false
     id("org.asciidoctor.jvm.convert") apply false
 }
 
@@ -31,20 +30,28 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-    apply(plugin = "org.jetbrains.kotlin.kapt")
-    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
-    apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
-    apply(plugin = "org.springframework.boot")
-    apply(plugin = "io.spring.dependency-management")
-    apply(plugin = "org.asciidoctor.jvm.convert")
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.kapt")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+        plugin("org.jetbrains.kotlin.plugin.jpa")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+    }
 
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
-        annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    }
+
+    tasks.getByName("bootJar") {
+        enabled = false
+    }
+
+    tasks.getByName("jar") {
+        enabled = true
     }
 
     tasks.withType<KotlinCompile> {
@@ -58,11 +65,4 @@ subprojects {
         useJUnitPlatform()
     }
 
-    tasks.withType<BootJar> {
-        mainClass.set("app.thunder.api")
-    }
-
-    tasks.getByName("asciidoctor") {
-
-    }
 }
